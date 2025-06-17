6825ed6eebb1fb8f8d06d7e0280560cc9882e92c
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-06-17 20:51:59 -0300

    Atualização

---
+++ reveal.js
@@ -0,0 +1,101 @@
+window.revelar = ScrollReveal({reset:false})
+
+revelar.reveal('.efeito-txt-topo' ,{
+duration: 1500,
+distance: '90px'
+})
+
+revelar.reveal('.efeito-img-topo' ,{
+    duration: 1500,
+    distance: '90px',
+    delay: 500
+    })
+
+revelar.reveal('.titulo' ,{
+        duration: 1500,
+        distance: '90px',
+        delay: 100
+        })
+
+revelar.reveal('.efeitos-especialidades1' ,{
+            duration: 2000,
+            distance: '90px',
+            delay: 500
+            })
+
+revelar.reveal('.efeitop' ,{
+            duration: 2000,
+            distance: '90px',
+            delay: 200
+            })
+
+revelar.reveal('.efeitos-especialidades2' ,{
+            duration: 2000,
+            distance: '90px',
+            delay: 1000
+            })
+
+revelar.reveal('.efeitos-especialidades3' ,{
+            duration: 2000,
+            distance: '90px',
+            delay: 1500
+            })
+
+revelar.reveal('.efeito-txt-sobre' ,{
+            duration: 2000,
+            distance: '90px',
+            delay: 1000,
+            origin: 'top'
+            })
+
+revelar.reveal('.efeito-img' ,{
+            duration: 1500,
+            distance: '90px',
+            delay: 500,
+            origin: 'left'
+            })
+
+
+revelar.reveal('.projetos1' ,{
+            duration: 2000,
+            distance: '90px',
+            delay: 500
+            })
+revelar.reveal('.projetos2' ,{
+            duration: 2000,
+            distance: '90px',
+            delay:1000
+            })
+revelar.reveal('.projetos3' ,{
+            duration: 2000,
+            distance: '90px',
+            delay: 1500
+            })
+
+
+revelar.reveal('.formulario1' ,{
+            duration: 2000,
+            distance: '90px',
+            delay: 2500
+            })
+revelar.reveal('.formulario2' ,{
+            duration: 2000,
+            distance: '90px',
+            delay: 500
+            })
+revelar.reveal('.formulario3' ,{
+            duration: 2000,
+            distance: '90px',
+            delay: 1000
+            })
+revelar.reveal('.formulario4' ,{
+            duration: 2000,
+            distance: '90px',
+            delay: 1500
+            })
+revelar.reveal('.formulario5' ,{
+            duration: 2000,
+            distance: '90px',
+            delay: 2000
+            })
+
